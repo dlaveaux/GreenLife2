5beeed4167a0c0b09c599ee679802d6715dc3e53
= <=>
2020-04-21 13:30:19 -0700

    home coms and the page done

---
+++ comps/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './home.css';
+
+
+const Home = ({ }) =>
+    <div id="whole">
+        <div id="homelogoid"></div>
+    </div>;
+
+// Home.defaultProps = {
+
+// }
+
+export default Home;
+
