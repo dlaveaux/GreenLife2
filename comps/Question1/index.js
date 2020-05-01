1fdcd07af3e076dc482eb99435e7d838f06a6931
= <=>
2020-05-01 02:44:04 -0700

    question com1 edit

--- comps/Question1/index.js
+++ comps/Question1/index.js
@@ -8,30 +8,35 @@ const pic1 = require('./pic1.png')
 const pic2 = require('./pic2.png')
 const pic3 = require('./pic3.png')
 
-const Question1 = ({question, text1, text2, text3}) =>
+const Question1 = ({question, text1, text2, text3, }) =>
     <div id="whole">
         <div id="questiontext">{question}</div>
         <div id="picid">
-            <div>
+            <div class="iconback first" >
                 <img src={pic1} />
                 <div class="text">{text1}</div>
             </div>
-            <div>
+            <div class="iconback second">
                 <img src={pic2} />
                 <div class="text">{text2}</div>
             </div>
-            <div>
+            <div class="iconback third">
                 <img src={pic3} />
                 <div class="text">{text3}</div>
             </div>
         </div>
     </div>;
 
+// function IconClick(){
+
+// }
+
 Question1.defaultProps = {
     question: "What are you wearing today?",
-    text1: "Recycled",
-    text2: "Wool/Cotton",
-    text3: "Polyester/Plastic"
+    text1: "1.Recycled",
+    text2: "2.Cotton",
+    text3: "3.Polyester",
+    // onClick: IconClick
 }
 
 export default Question1;
