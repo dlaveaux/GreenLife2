255b170e062bea0f0be1dcd817ae4b832e0e272b
= <=>
2020-05-03 08:08:15 -0700

    made foodtip page

--- comps/FoodTip/index.js
+++ comps/FoodTip/index.js
@@ -1,20 +1,33 @@
 import React from 'react';
-// import './tipicons.css';
+import './foodtip.css';
 
 const pic1= require('../TipIcons/food.png');
 // const pic2= require('./clothing.png');
 // const pic3= require('./car.png');
 
-const FoodTip = ({}) =>
-<div id="tipborder">
-    <div class="tipsicons one"><img src={pic1}/>Food</div>
+const FoodTip = ({text1, text2, text3, text4, text5}) =>
+<div id="foodtipborder">
+    <div id="foodtipimagearea">
+        <img src={pic1}/>
+        <span id="foodtiptitle">Eating homecooked meals..</span>
+    </div>
+    <span id="foodtiptext">
+    {text1}<br></br>
+    {text2}<br></br>
+    {text3}<br></br>
+    {text4}<br></br>
+    {text5}<br></br></span>
     {/* <div class="tipsicons two"><img src={pic2}/>Clothing</div>
     <div class="tipsicons three"><img src={pic3}/>Transportation</div> */}
 </div>;
 
-// TipsIcons.defaultProps = {
-//     profpic:defaultProfPic,
-// }
+FoodTip.defaultProps = {
+    text1: "Saves money",
+    text2: "Is Healty",
+    text3: "Is Sustainable",
+    text4: "Is good for the planet",
+    text5: "Improves your cooking"
+}
 
 export default FoodTip;
 
